a9704c347b25026a1a89bf1da713cb5d72724d06
dallmann <dallmann>
2000-09-12 19:08:05 +0000

    initial library for applications

---
+++ lib/print_hex8.s
@@ -0,0 +1,38 @@
+		;; for emacs: -*- MODE: asm; tab-width: 4; -*-
+		
+		;; print_hex8
+		;; prints 8bit-value hexadecimal to stdout
+
+		;; < A=value
+		;; > (A,X,Y=XX)
+
+#define _date (Jun 15 1997)
+#include <stdio.h>
+
+.global print_hex8
+
+		.byte $0c
+		.word print_hex8
+hex_tab:
+		.text "0123456789abcdef"
+
+print_hex8:
+		pha
+		lsr  a
+		lsr  a
+		lsr  a
+		lsr  a
+		jsr  +
+		
+		pla
+		and  #$0f
+	+	tay
+		lda  hex_tab,y
+		sec
+		ldx  #stdout
+		jsr  fputc
+		bcs  +
+		rts
+
+	+	jmp  lkf_catcherr
+
