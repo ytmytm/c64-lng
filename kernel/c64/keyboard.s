bbdc6b0417b712b01143c1136a5316a302b6b9b3
ytm <ytm>
2001-02-24 23:29:52 +0000

    more shared code in keyboard.s

--- kernel/c64/keyboard.s
+++ kernel/c64/keyboard.s
@@ -483,21 +483,3 @@ to_toggle_console:
 	+	jmp  console_toggle			; call function of console driver
 							; (console_toggle is defined is console.s)
 
-
-		;; get state of keyboard
-keyb_stat:
-		lda  altflags				; bit2..0= CTRL,right_SHIFT,left_SHIFT
-		rts
-
-		;; get state of joystick 0
-keyb_joy0:
-		lda  joy0result
-		eor  #$ff
-		rts
-
-		;; get state of joystick 1
-keyb_joy1:
-		lda  joy1result
-		eor  #$ff
-		rts
-
--- kernel/keyboard.s
+++ kernel/keyboard.s
@@ -12,3 +12,20 @@
 #else
 # include MACHINE(keyboard.s)
 #endif
+
+		;; get state of keyboard
+keyb_stat:
+		lda  altflags				; bit2..0= CTRL,right_SHIFT,left_SHIFT
+		rts
+
+		;; get state of joystick 0
+keyb_joy0:
+		lda  joy0result
+		eor  #$ff
+		rts
+
+		;; get state of joystick 1
+keyb_joy1:
+		lda  joy1result
+		eor  #$ff
+		rts
